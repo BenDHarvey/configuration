c9b412ce3d016ed2ea6281cfb061e8c3bd98e204
Ben <benharvey@posteo.net>
2021-03-30 05:23:07 +1100

    Add some more git repos

--- git/bendharvey.tf
+++ git/bendharvey.tf
@@ -12,3 +12,17 @@ resource "github_repository" "org" {
 
   visibility = "private"
 }
+
+resource "github_repository" "bloomsprout_infrastructure" {
+  name        = "bloomsprout-infrastructure"
+  description = "Contains infrastructure code for the bloomsprout project"
+
+  visibility = "public"
+}
+
+resource "github_repository" "bloomsprout_temperature_sensor" {
+  name        = "bloomsprout-temperature-sensor"
+  description = "Contains code for the temperatures sensor (DHT11)"
+
+  visibility = "public"
+}
