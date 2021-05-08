2e76fdd7fa17025e61131c8a4f0be81e2c343461
Ben Harvey <ben@harvey.onl>
2021-05-09 07:23:55 +1000

    * Moves server infra into its own repo

--- cloud/linode/terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-resource "local_file" "kubeconfig" {
-  depends_on = [linode_lke_cluster.control_cluster]
-  filename   = "kube-config"
-  content    = base64decode(linode_lke_cluster.control_cluster.kubeconfig)
-}
--- scripts/destroy_cloud_terraform.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Script to deploy cloud terraform asssets
-
-export TF_VAR_LINODE_TOKEN=$(sops -d --extract '["linode_token"]' ./secrets/linode.yaml)
-
-cd cloud/linode/terraform
-
-terraform destroy
