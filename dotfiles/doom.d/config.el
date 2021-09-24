274376b1aad202bb424cf77e103475e25fbd1d04
Ben Harvey <81058992+BenDHarvey@users.noreply.github.com>
2021-09-24 11:12:26 +1000

    move into a single setq block

--- dotfiles/doom.d/config.el
+++ dotfiles/doom.d/config.el
@@ -1,25 +1,23 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 (setq user-full-name "Ben Harvey"
-      user-mail-address "ben@harvey.onl")
+      user-mail-address "ben@harvey.onl"
+      doom-font (font-spec :family "monospace" :size 14)
+      doom-theme 'doom-acario-dark
 
-(setq doom-font (font-spec :family "monospace" :size 14))
+      org-directory "~/Documents/org"
+      display-line-numbers-type 'relative
+      auth-sources '("~/.authinfo.gpg")
 
-(setq doom-theme 'doom-acario-dark)
+      evil-shift-width 2
+      tab-width 2
 
-(setq org-directory "~/Documents/org")
-
-(setq display-line-numbers-type 'relative)
-
-(setq auth-sources '("~/.authinfo.gpg"))
+      ;; Using alt key does not working mac os. Hack to get the right alt key working
+      mac-option-modifier nil
+      mac-command-modifier 'meta)
 
 (setq-default evil-shift-width 2 ;; I normally use 2wide for my projects.
               tab-width 2)
-
-
-;; Using alt key does not working mac os. Hack to get the right alt key working
-(setq mac-option-modifier nil
-      mac-command-modifier 'meta)
 
 ;; Module imports
 (load! "+org")     ;; Org mode stuff like todos and rebindings
