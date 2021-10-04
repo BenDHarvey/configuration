8ccbc280b63c4eba5c8e1f2f1fb13fa633a8ea25
Ben Harvey <81058992+BenDHarvey@users.noreply.github.com>
2021-10-04 21:53:15 +1100

    adds git config into nix

--- nix/home.nix
+++ nix/home.nix
@@ -341,10 +341,25 @@ in
       };
     };
 
-#    git = {
-#      enable = true;
-#      username = "BenDHarvey";
-#    };
+    git = {
+      enable = true;
+      userName = "Ben Harvey";
+      userEmail = "ben@harvey.onl";
+      includes = [{
+        condition = "gitdir:~/Workspace/github.com/BenDHarvey/**";
+        contents = {
+          user.email = "ben@harvey.onl";
+          user.name = "Ben Harvey";
+        };
+      }
+      {
+        condition = "gitdir:~/Workspace/github.com/BMLOnline/**";
+        contents = {
+          user.email = "benh@bodymindlife.online";
+          user.name = "Ben Harvey";
+        };
+      }];
+    };
 
     tmux = {
         enable = true;
