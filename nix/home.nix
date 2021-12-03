94313b8a24ba97bd13c670906c07b45e83473313
Ben Harvey <81058992+BenDHarvey@users.noreply.github.com>
2021-12-04 05:19:54 +1100

    update the font size and add a couple of alacritty things

--- nix/home.nix
+++ nix/home.nix
@@ -102,8 +102,12 @@ in
     alacritty = {
       enable = true;
       settings = {
-        font.size = 11;
-#        shell.program = "/usr/local/bin/fish";
+        font.size = 12;
+        normal = {
+          family = "Iosevka Nerd Font";
+          style = "Regular";
+        };
+        mouse.hide_when_typing = true;
       };
     };
 
