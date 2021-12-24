1ce850158648f9420728a7937ac1348f3d316f22
Ben Harvey <81058992+BenDHarvey@users.noreply.github.com>
2021-12-25 05:32:21 +1100

    add some config

--- nix/modules/home/home.nix
+++ nix/modules/home/home.nix
@@ -5,7 +5,9 @@ with builtins;
 with lib;
 with import <home-manager/modules/lib/dag.nix> { inherit lib; };
 
-{
+let
+  settings = import ./config;
+in {
   imports = [
 #    <home-manager/nix-darwin>
     ./shared/home.nix
