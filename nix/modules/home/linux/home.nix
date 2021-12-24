10cefbbac1523d6c8738126fedc9690a728bce61
Ben Harvey <81058992+BenDHarvey@users.noreply.github.com>
2021-12-25 05:52:30 +1100

    typo

--- nix/modules/home/linux/home.nix
+++ nix/modules/home/linux/home.nix
@@ -98,6 +98,6 @@
   services.unclutter.enable = true;
 
   home.file.".config/i3/config".source = ./i3/i3_config;
-  home.file.".config/i3/bar".source = ./i3/i3_status_rs";
+  home.file.".config/i3/bar".source = ./i3/i3_status_rs;
   home.file.".local/share/rofi/themes/base16-dracula.rasi".source = ./files/rofi_theme;
 }
