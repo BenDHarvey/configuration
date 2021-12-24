a71356f8478d2de33151d816b86c9dafacd5ecc0
Ben Harvey <81058992+BenDHarvey@users.noreply.github.com>
2021-12-24 14:25:43 +1100

    change relative path

--- nix/modules/home/shared/home.nix
+++ nix/modules/home/shared/home.nix
@@ -9,7 +9,7 @@ let
   doom-emacs = pkgs.callPackage (builtins.fetchTarball {
     url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
   }) {
-    doomPrivateDir = ../dotfiles/doom.d;  # Directory containing your config.el init.el
+    doomPrivateDir = ../../../dotfiles/doom.d;  # Directory containing your config.el init.el
   };
 in 
 
