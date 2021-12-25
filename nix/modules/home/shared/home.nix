e1439c05a74d36ee00120b871ba4a5e360d26d3d
Ben Harvey <ben@harvey.onl>
2021-12-26 10:35:17 +1100

    some small tweaks to default terminal settings

--- nix/modules/home/shared/home.nix
+++ nix/modules/home/shared/home.nix
@@ -82,12 +82,13 @@ with lib;
     alacritty = {
       enable = true;
       settings = {
-        font.size = 12;
+        font.size = 11;
         normal = {
           family = "Iosevka Nerd Font";
           style = "Regular";
         };
         mouse.hide_when_typing = true;
+        shell.program = "/home/ben/.nix-profile/bin/zsh";
       };
     };
 
