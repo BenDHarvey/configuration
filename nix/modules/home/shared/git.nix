aaeba62b40a3ca1c5f759c510d98d9ff9780f10f
Ben Harvey <ben@harvey.onl>
2021-12-26 09:14:27 +1100

    formats the buffer correctly

--- nix/modules/home/shared/git.nix
+++ nix/modules/home/shared/git.nix
@@ -1,35 +1,32 @@
 { config, lib, pkgs, ... }:
 
-with import <nixpkgs> {};
+with import <nixpkgs> { };
 
 {
-    home = {
-        packages = with pkgs; [
-            mu
-            isync
-        ];
+  home = { packages = with pkgs; [ mu isync ]; };
+
+  programs = {
+
+    git = {
+      enable = true;
+      userName = "Ben Harvey";
+      userEmail = "ben@harvey.onl";
+      includes = [
+        {
+          condition = "gitdir:~/Workspace/github.com/BenDHarvey/**";
+          contents = {
+            user.email = "ben@harvey.onl";
+            user.name = "Ben Harvey";
+          };
+        }
+        {
+          condition = "gitdir:~/Workspace/github.com/BMLOnline/**";
+          contents = {
+            user.email = "benh@bodymindlife.online";
+            user.name = "Ben Harvey";
+          };
+        }
+      ];
     };
-
-    programs = {
-
-        git = {
-            enable = true;
-            userName = "Ben Harvey";
-            userEmail = "ben@harvey.onl";
-            includes = [{
-                    condition = "gitdir:~/Workspace/github.com/BenDHarvey/**";
-                    contents = {
-                    user.email = "ben@harvey.onl";
-                    user.name = "Ben Harvey";
-                };
-            }
-            {   
-                    condition = "gitdir:~/Workspace/github.com/BMLOnline/**";
-                    contents = {
-                    user.email = "benh@bodymindlife.online";
-                    user.name = "Ben Harvey";
-                };
-            }];
-            };
-        };
+  };
 }
