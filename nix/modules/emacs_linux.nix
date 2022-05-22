b4d691055498fca42c1687a88824ea9fa610eda2
Ben Harvey <ben@harvey.onl>
2022-05-23 09:01:05 +1000

    [WIP] Get emacs in linux working
    Couple of small updates to get emacs on linux working. Adds a little
    snippet so that the gui version can be launched from the application seletor

--- nix/modules/emacs_linux.nix
+++ nix/modules/emacs_linux.nix
@@ -2,15 +2,12 @@
 
 {
   services.emacs.enable = true;
+  programs.emacs.enable = true;
 
   home = {
-    programs.emacs = {
-      enable = true;
-    };
 
     # Install the needed emacs packages
     packages = with pkgs; [
-      emacs
       emacs-all-the-icons-fonts
       #emacs27Packages.pdf-tools
     ];
@@ -18,4 +15,20 @@
     # Link doom config files to the correct location
     file.".doom.d".source = ../../dotfiles/doom.d;
   };
+
+  # Got this nice little snippet from here https://github.com/little-dude/dotfiles/blob/master/programs/emacs/default.nix#L109
+  xdg.dataFile."applications/emacsclient.desktop".text = ''
+    [Desktop Entry]
+    Name=Emacsclient
+    GenericName=Text Editor
+    Comment=Edit text
+    MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;
+    Exec=emacsclient -c -a emacs %F
+    Icon=emacs
+    Type=Application
+    Terminal=false
+    Categories=Development;TextEditor;
+    StartupWMClass=Emacs
+    Keywords=Text;Editor;
+  '';
 }
