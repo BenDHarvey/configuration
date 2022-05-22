da3501df3efc0b3b9584a411b73d43e6f4f9b749
Ben <ben@pop-os.localdomain>
2022-05-22 11:21:42 +1000

    update bootstrap scripts a little

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,12 @@
 
 source ./shared_bootstrap_funcs.sh
 
-apt_update_system()
-clone_important_repos()
-install_nix()
-install_homemanager()
-link_config_files()
-install_homemanager_post()
-install_emacs()
-install_addition_linux_packages()
-install_docker()
+apt_update_system
+clone_important_repos
+install_nix
+install_homemanager
+link_config_files
+install_homemanager_post
+#install_emacs()
+#install_addition_linux_packages()
+#install_docker()
--- shared_bootstrap_funcs.sh
+++ shared_bootstrap_funcs.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/env bash
 
 apt_update_system() {
+  echo "=== Starting system update ==="
   sudo apt update && sudo apt upgrade -y
+
+  echo ""
+  echo "=== Done ==="
+  echo ""
 }
 
 clone_important_repos() {
+  echo "=== Starting repo clone ==="
   git clone git@github.com:BenDHarvey/org.git ~/Documents/org
+  
+  echo ""
+  echo "=== Done ==="
+  echo ""
 }
 
 install_nix() {
+  echo "=== Starting nix install ==="
   # Install nix
   curl -L https://nixos.org/nix/install | sh
 
   # source the nix shell script
   . /home/ben/.nix-profile/etc/profile.d/nix.sh
+
+  echo ""
+  echo "=== Done ==="
+  echo ""
 }
 
 install_homemanager() {
+  echo "=== Starting nix home-manager install ==="
   # Install home-manager
   nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   nix-channel --update
@@ -25,33 +41,55 @@ install_homemanager() {
   echo 'export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH' >> ~/.bashrc
 
   nix-shell '<home-manager>' -A install
+
+  echo ""
+  echo "=== Done ==="
+  echo ""
 }
 
 install_nix_darwin() {
+  echo "=== Starting nix darwin install ==="
   # Install nix-darwin on the system
   # This also comes straight from the nix-darwin github page
   nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
   ./result/bin/darwin-installer
+  echo ""
+  echo "=== Done ==="
+  echo ""
 }
 
 install_homemanager_post() {
+  echo "=== Starting nix home-manager post intall steps ==="
   # Apply the home-manager config
   home-manager switch -b backup
 
   # Make zsh the default terminal
   sudo chsh -s $(which zsh)
+  echo ""
+  echo "=== Done ==="
+  echo ""
 }
 
 link_config_files() {
+  echo "=== Starting link config files ==="
   # Link default.nix file from .configuration repo to local home directory
   rm ~/.config/nixpkgs/home.nix || true
   rm ~/.config/nixpkgs/config.nix || true
   ln -s ~/.configuration/nix/home.nix ~/.config/nixpkgs/home.nix || true
   ln -s ~/.configuration/nix/config.nix ~/.config/nixpkgs/config.nix || true
+
+  echo ""
+  echo "=== Done ==="
+  echo ""
 }
 
 install_addition_linux_packages() {
-    sudo apt install -y i3 \
+  echo "=== Starting link config files ==="
+
+  sudo add-apt-repository ppa:regolith-linux/release
+  sudo apt-get update
+ 
+    sudo apt install -y i3-gaps \
         libsqlite3-dev \
         sqlite3 \
         ruby \
@@ -73,12 +111,21 @@ install_addition_linux_packages() {
   sudo apt install pgadmin4 -y
 
   sudo gem install rails
+
+  echo ""
+  echo "=== Done ==="
+  echo ""
 }
 
 install_emacs() {
+  echo "=== Starting doom emacs install ==="
   # Install doom emacs
   git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
   ~/.emacs.d/bin/doom install
+
+  echo ""
+  echo "=== Done ==="
+  echo ""
 }
 
 
